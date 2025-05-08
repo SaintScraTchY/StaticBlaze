119ce1b0881492e907136ddb1727790ab92de9ae
SaintScraTchY <mehrshad2028@gmail.com>
2025-05-09 00:51:25 +0330

    Update blog asset paths to reflect new directory structure

--- StaticBlazeWASM/Constants/StaticBlazeConfig.cs
+++ StaticBlazeWASM/Constants/StaticBlazeConfig.cs
@@ -5,11 +5,11 @@ public static class StaticBlazeConfig
     public static string BaseUrl { get; set; }
     
     public const string ImagePattern = @"!\[.*?\]\(data:image\/(png|jpeg|jpg|gif);base64,([A-Za-z0-9+/=]+)\)";
-    public const string BlogAssets = @"StaticBlazeWASM/Blog/assets";
-    public const string BlogCategories = @"StaticBlazeWASM/Blog/Categories";
-    public const string BlogTags = @"StaticBlazeWASM/Blog/Tags";
-    public const string BlogPosts = @"StaticBlazeWASM/Blog/Posts";
-    public const string BlogDocs = @"StaticBlazeWASM/Blog/Docs";
-    public const string BlogPages = @"StaticBlazeWASM/Blog/Pages";
-    public const string BlogDB = @"StaticBlazeWASM/Blog/Persistence";
+    public const string BlogAssets = @"StaticBlazeWASM/wwwroot/Static/Blog/assets";
+    public const string BlogCategories = @"StaticBlazeWASM/wwwroot/Static/Blog/Categories";
+    public const string BlogTags = @"StaticBlazeWASM/wwwroot/Static/Blog/Tags";
+    public const string BlogPosts = @"StaticBlazeWASM/wwwroot/Static/Blog/Posts";
+    public const string BlogDocs = @"StaticBlazeWASM/wwwroot/Static/Blog/Docs";
+    public const string BlogPages = @"StaticBlazeWASM/wwwroot/Static/Blog/Pages";
+    public const string BlogDB = @"StaticBlazeWASM/wwwroot/Static/Blog/Persistence";
 }
